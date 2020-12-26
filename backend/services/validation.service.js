52c82dc9d1ed0318125d3ef6aeb37128eeee0731
bandura <banduraweb@ukr.net>
2020-12-26 19:45:50 +0200

    do user registration && login

---
+++ backend/services/validation.service.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+class Validation {
+  static registrationValidation(body) {
+    const schema = Joi.object({
+      name: Joi.string().min(3).required(),
+      email: Joi.string().min(3).required().email(),
+      password: Joi.string().min(6).required(),
+    });
+    return schema.validate(body);
+  }
+
+  static loginValidation(body) {
+    const schema = Joi.object({
+      email: Joi.string().min(3).required().email(),
+      password: Joi.string().min(6).required(),
+    });
+    return schema.validate(body);
+  }
+}
+
+module.exports = Validation;
