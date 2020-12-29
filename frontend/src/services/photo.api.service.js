57e34d9d66dcbc9d931b743f84c97be19fdffe6a
bandura <banduraweb@ukr.net>
2020-12-29 08:42:01 +0200

    fix url

--- frontend/src/services/photo.api.service.js
+++ frontend/src/services/photo.api.service.js
@@ -4,7 +4,7 @@ const pixabayKey = '11370902-19d6d747d66b3dd76f6049b2b';
 
 const get = (payload) =>
   api.get(
-    `/api?key=${pixabayKey}&q=${payload.keyword}&image_type=photo&per_page=12&page=${payload.pageNumber}`
+    `/api/?key=${pixabayKey}&q=${payload.keyword}&image_type=photo&per_page=12&page=${payload.pageNumber}`
   );
 
 const PhotoApi = {
